c205a941fec0fe81675970235466ae6688e7bac3
mubeena12 <mubeena.begum043@gmail.com>
2023-12-11 16:26:07 -0700

    Rename the start-stop script

--- jetson-nano-2gb-devkit/meta-docker-app/recipes-containers/docker-app/files/webapp-start-stop.sh
+++ jetson-nano-2gb-devkit/meta-docker-app/recipes-containers/docker-app/files/webapp-start-stop.sh
@@ -8,13 +8,16 @@ case "$1" in
     start)
         echo "Starting postgres and webapp..."
         # Run the docker containers for postgres and webapp
-	cd /usr/bin
-         docker-compose up -d
+        # cd /usr/bin
+        #docker-compose up -d
+        sleep 300
+        cd /usr/bin && start-stop-daemon -v -S -n docker-compose -a /usr/bin/docker-compose -- up -d
         ;;
     stop)
         echo "Stopping postgres and webapp..."
-	cd /usr/bin
-        docker-compose stop
+        #cd /usr/bin
+        #docker-compose stop
+        cd /usr/bin && start-stop-daemon -v -K -n docker-compose -a /usr/bin/docker-compose -- stop
         ;;
     *)
         echo "Usage: $0 {start|stop}"
